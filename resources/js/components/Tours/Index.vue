0f6c714a6b5eff0bd81f84d13f4a5afb536fe668
Sachini Wijesinghe <wijesinghesachini09@gmail.com>
2025-09-10 09:58:51 +0530

    #2 Feat: Add vuetufy to laravel project

--- resources/js/components/Tours/Index.vue
+++ resources/js/components/Tours/Index.vue
@@ -1,4 +1,9 @@
 <template>
-  <h2 class="text-lg font-semibold">example</h2>
-
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn color="primary">Hello Vuetify!</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
